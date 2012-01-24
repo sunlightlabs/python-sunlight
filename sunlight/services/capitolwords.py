a2554894faa4327437ced2a417a6ccebc40781ee
Paul Tagliamonte <paultag@sunlightfoundation.com>
2012-01-24 17:56:45 -0500

    Adding in CW docstrings

--- sunlight/services/capitolwords.py
+++ sunlight/services/capitolwords.py
@@ -26,14 +26,30 @@ module_name = "capitolwords"
 service_url = "http://capitolwords.org/api"
 
 class CapitolWords(sunlight.service.Service):
+    """
+    Bindings into the CapitolWords project
+
+    API Ref:            http://capitolwords.org/api/
+    About the project:  http://capitolwords.org/api/
+    """
 
     def dates( self, **kwargs ):
+        """
+        Find the popularity of a phrase over a period of time.
+        Standard arguments are supported.
+        """
         return self.get( "dates", **kwargs )
 
     def phrases( self, **kwargs ):
+        """
+        List the top phrases for a facet.
+        """
         return self.get( "phrases", **kwargs )
 
     def text( self, **kwargs ):
+        """
+        Full-text search. Standard arguments are supported
+        """
         return self.get( "text", **kwargs )
 
     def get_url( self, obj, apikey, **kwargs ):
