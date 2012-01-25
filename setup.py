845c845a658bd9de40cf1841b2d991422749922b
Paul Tagliamonte <paultag@sunlightfoundation.com>
2012-01-25 11:02:14 -0500

    Last little tweek to the setup file

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python
 
 from sunlight import __appname__, __version__
-
 from distutils.core import setup
+
+long_description = open('README.rst').read()
 
 setup(
 	name       = __appname__,
 	version    = __version__,
-	packages   = [ 'sunlight', 'sunlight.services' ]
+	packages   = [ 'sunlight', 'sunlight.services' ],
+
+    author       = "Paul Tagliamonte",
+    author_email = "paultag@sunlightfoundation.com",
+
+    long_description = long_description,
+    description      = 'Unified Sunlight API bindings',
+    license          = "Expat",
+    url              = "https://github.com/paultag/python-sunlight",
+
+    platforms        = ['any']
 )
