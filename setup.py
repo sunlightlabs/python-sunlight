1851b8b6a5ad1dce537f94dc03f38021bda467ad
Paul Tagliamonte <paultag@sunlightfoundation.com>
2012-01-25 10:58:31 -0500

    fixing up the setup

--- setup.py
+++ setup.py
@@ -2,21 +2,10 @@
 
 from sunlight import __appname__, __version__
 
-import os
-from os.path import join
 from distutils.core import setup
-
-def dirls(d):
-	ret = []
-	for root, dirs, files in os.walk(d):
-		if files != []:
-			for f in files:
-				ret.append(root + "/" + f)
-	return ret
 
 setup(
 	name       = __appname__,
 	version    = __version__,
-	packages   = [ 'sunlight', 'sunlight.services' ],
-	scripts    = dirls('bin')
+	packages   = [ 'sunlight', 'sunlight.services' ]
 )
