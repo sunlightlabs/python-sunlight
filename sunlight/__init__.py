fa4db88186b795b2259650096d73fe9408861299
Paul Tagliamonte <paultag@sunlightfoundation.com>
2012-02-21 16:43:03 -0500

    Releasing 1.0.5

--- sunlight/__init__.py
+++ sunlight/__init__.py
@@ -11,7 +11,7 @@ fight with finding the right module to use.
 """
 
 __appname__ = "sunlight"
-__version__ = "1.0.4"
+__version__ = "1.0.5"
 
 import sunlight.services.openstates
 import sunlight.services.capitolwords
