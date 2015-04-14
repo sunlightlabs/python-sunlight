3dc645949469e857c961f4a26025c18337c61131
James Turk <james.p.turk@gmail.com>
2015-04-14 17:34:52 -0400

    add classifiers to python-sunlight

--- setup.py
+++ setup.py
@@ -23,5 +23,12 @@ setup(
     license          = "BSD",
     url              = "https://github.com/sunlightlabs/python-sunlight",
 
-    platforms        = ['any']
+    platforms        = ['any'],
+    
+    classifiers      = ['Programming Language :: Python',
+                        'Programming Language :: Python :: 2',
+                        'Programming Language :: Python :: 2.7',
+                        'Programming Language :: Python :: 3',
+                        'Programming Language :: Python :: 3.3',
+                        'Programming Language :: Python :: 3.4'],
 )
